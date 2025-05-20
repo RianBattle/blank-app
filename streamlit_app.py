ca1ac92dd572826e0a1313d21829b812b5cef7df
Rian Battle <89739973+RianBattle@users.noreply.github.com>
2025-05-20 15:24:54 +0000

    more examples

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,100 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import time
 
 st.title("🎈 My new Streamlit app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+df = pd.DataFrame({
+    'first column': [1,2,3,4],
+    'second column': [10,20,30,40]
+})
+
+df
+
+option = st.selectbox(
+    'Which number do you like best?',
+    df['first column']
+)
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20))
+)
+st.dataframe(dataframe.style.highlight_max(axis=0))
+
+if st.checkbox('Show line chart dataframe', True):
+    chart_data = pd.DataFrame(
+        np.random.randn(20, 3),
+        columns=['a', 'b', 'c']
+    )
+    st.line_chart(chart_data)
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon']
+)
+st.map(map_data)
+
+x = st.slider('x')
+st.write(x, 'squared is', x * x)
+
+st.text_input("Your name", key="name")
+st.session_state.name
+
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
+
+left_column, right_column = st.columns(2)
+left_column.button('Press me!')
+
+with right_column:
+    chosen = st.radio(
+        'Sorting hat',
+        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin")
+    )
+    st.write(f"You are in {chosen} house!")
+
+'Starting a long computation'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i + 1)
+    time.sleep(0.01)
+
+'...and now we\'re done!'
+
+@st.cache_data
+def long_running_function(param1, param2):
+    return _
+
+if "counter" not in st.session_state:
+    st.session_state.counter = 0
+
+st.session_state.counter += 1
+
+st.header(f"This page has run {st.session_state.counter} times.")
+st.button("Run it again")
+
+if "df" not in st.session_state:
+    st.session_state.df = pd.DataFrame(
+        np.random.randn(20, 2),
+        columns=["x", "y"]
+    )
+
+st.header("Choose a datapoint color")
+color = st.color_picker("Color", "#FF0000")
+st.divider()
+st.scatter_chart(st.session_state.df, x="x", y="y", color=color)
